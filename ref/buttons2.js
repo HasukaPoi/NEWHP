eb50bdb71a75310338f379f041dfb4a4c6f0793a
HasukaPc <hasukapyon@163.com>
2018-01-23 13:51:31 +0800

    search box added. updated css.

--- ref/buttons2.js
+++ ref/buttons2.js
@@ -2,3 +2,16 @@
  * Created by Hasuka on 2018/1/20.
  * The js of buttons in newhp2.html
  */
+
+$(document).ready(function () {
+    $("#bing").click(function () {
+        window.open('https://www.bing.com/search?q=' + $("#webSearchKey").val());
+    });
+    $('#yahoo').click(function () {
+        window.open('http://search.yahoo.co.jp/search' + $("#webSearchKey").val());
+    });
+    $('#webSearchKey').keydown(function (e) {
+        var curKey = e.which;
+        if (curKey === 13) $("#bing").click();
+    });
+});
