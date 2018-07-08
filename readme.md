14e0b27ab5ac5194dcf2741eaf62b6368679d909
Hasuka <hasukapyon@163.com>
2018-07-08 23:32:21 +0800

    Update

--- readme.md
+++ readme.md
@@ -2,9 +2,9 @@
 A navigation page which made of and for myself.
 
 ## Overview
-`index.html` - Current homepage on my PC.
+`index.html` - New guy, under cunstruction. Partially as rewrite of `index.html`.
 
-`newhp2.html` - New guy, under cunstruction. Partially as rewrite of `index.html`.
+`indexOLD.html` - Old ver.
 
 ## References
 Bootstrap (not-pretty usage)
--- ref/buttons2.js
+++ ref/buttons2.js
@@ -8,10 +8,22 @@ $(document).ready(function () {
         window.open('https://www.bing.com/search?q=' + $("#webSearchKey").val());
     });
     $('#yahoo').click(function () {
-        window.open('http://search.yahoo.co.jp/search' + $("#webSearchKey").val());
+        window.open(encodeURI('http://search.yahoo.co.jp/search?p=' + $("#webSearchKey").val()));
     });
     $('#webSearchKey').keydown(function (e) {
         var curKey = e.which;
         if (curKey === 13) $("#bing").click();
+    });
+
+
+    $("#weblio").click(function () {
+        window.open('http://www.weblio.jp/content/' + $("#dictSearchKey").val());
+    });
+    $('#goo').click(function () {
+        window.open('https://dictionary.goo.ne.jp/srch/all/' + $("#dictSearchKey").val()+ '/m0u/');
+    });
+    $('#dictSearchKey').keydown(function (e) {
+        var curKey = e.which;
+        if (curKey === 13) $("#weblio").click();
     });
 });
