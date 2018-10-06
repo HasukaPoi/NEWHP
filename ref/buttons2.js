31009e6ded86059fb02ff7535af2724690c1ac38
Hasuka <hasukapyon@163.com>
2018-10-07 01:31:39 +0800

    add some searchs

--- ref/buttons2.js
+++ ref/buttons2.js
@@ -20,10 +20,38 @@ $(document).ready(function () {
         window.open('http://www.weblio.jp/content/' + $("#dictSearchKey").val());
     });
     $('#goo').click(function () {
-        window.open('https://dictionary.goo.ne.jp/srch/all/' + $("#dictSearchKey").val()+ '/m0u/');
+        window.open('https://dictionary.goo.ne.jp/srch/all/' + $("#dictSearchKey").val() + '/m0u/');
     });
     $('#dictSearchKey').keydown(function (e) {
         var curKey = e.which;
         if (curKey === 13) $("#weblio").click();
+    });
+
+    $("#taobao").click(function () {
+        if ($("#buySearchKey").val() == '')
+            window.open('https://www.taobao.com');
+        else
+            window.open('https://s.taobao.com/search?q=' + $("#buySearchKey").val());
+    });
+    $('#jd').click(function () {
+        if ($("#buySearchKey").val() == '')
+            window.open('https://www.jd.com');
+        else
+            window.open('https://search.jd.com/Search?keyword=' + $("#buySearchKey").val());
+    });
+    $('#buySearchKey').keydown(function (e) {
+        var curKey = e.which;
+        if (curKey === 13) $("#taobao").click();
+    });
+
+    $("#erotoku").click(function () {
+        window.open('http://erogame-tokuten.com/redate.php?keyword=' + $("#erogeSearchKey").val().replace(/ /g, "+"));
+    }); 
+    $("#getchu").click(function () {
+        window.open('http://www.getchu.com/php/nsearch.phtml?genre=pc_soft&search_keyword=' + $("#erogeSearchKey").val().replace(/ /g, "+"));
+    });
+    $('#erogeSearchKey').keydown(function (e) {
+        var curKey = e.which;
+        if (curKey === 13) $("#erotoku").click();
     });
 });
